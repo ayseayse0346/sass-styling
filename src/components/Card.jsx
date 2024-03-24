48add01c5b9e99158b04e4238b8059d5dde03009
ayseayse0346 <ayseayse.ayse0346@gmail.com>
2024-03-24 16:45:01 +0300

    add

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-
+import CardStyle from "../sass/card.module.scss";
 const Card = ({name,job,comment,img}) => {
   return (
     <div className='container'>
-        <div className='card'>
+        <div className={CardStyle["card"]}>
             <h1>{name}</h1>
             <h3>{job}</h3>
             <p>{comment}</p>
             <img src={img} alt={name} />
-            <div className='buttons'>
-                <button className='small'>Small</button>
-                <button className='large'>Large</button>
+            <div className={CardStyle["buttons"]}>
+                <button className={CardStyle["small"]}>Small</button>
+                <button className={CardStyle["large"]}>Large</button>
             </div>
         </div>
     </div>
